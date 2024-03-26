50b14404d63d134db841514dfef875f92114482c
Robert Kaurin <robykaurin@gmail.com>
2024-03-26 13:48:01 +0100

    add font and rpm counter w test data

--- App.tsx
+++ App.tsx
@@ -8,12 +8,14 @@
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  Image,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
+  useWindowDimensions,
   View,
 } from 'react-native';
 
@@ -61,42 +63,79 @@ function App(props: any): React.JSX.Element {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
-
+  const {width, height} = useWindowDimensions();
+  console.log(width, height);
+  const testData = [1600];
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
+    // <SafeAreaView style={backgroundStyle}>
+    //   <ScrollView
+    //     contentInsetAdjustmentBehavior="automatic"
+    //     style={backgroundStyle}>
+    //     <View
+    //       style={{
+    //         backgroundColor: isDarkMode ? Colors.black : Colors.white,
+    //       }}>
+    //       <Section title="Step One">
+    //         Edit <Text style={styles.highlight}>{props.counter}</Text>
+    //       </Section>
+    //       <Section title="See Your Changes">
+    //         {props.doubleArray?.length && (
+    //           <>
+    //             <Text style={styles.rpmBig}>
+    //               {String(props.doubleArray[0])[0]}
+    //             </Text>
+    //             <Text style={styles.rpm}>
+    //               {String(Math.round(props.doubleArray[0] / 10) * 10).slice(1)}
+    //             </Text>
+    //           </>
+    //         )}
+    //       </Section>
+    //       <Section title="Debug">
+    //         is ecu connected: {`${props.isConnected}`}
+    //       </Section>
+    //     </View>
+    //   </ScrollView>
+    // </SafeAreaView>
+    <View style={{position: 'relative', flex: 1}}>
+      <Image
+        source={background}
+        style={{
+          position: 'absolute',
+          top: 0,
+          width: width,
+          height: height,
+          left: 0,
+        }}></Image>
+      <Image
+        source={gauge}
+        style={{
+          position: 'absolute',
+          top: height * 0.5 - height / 1.7335 / 2,
+          left: width * 0.5 - width / 2.8125 / 2,
+          width: width / 2.8125,
+          height: height / 1.7335,
+        }}></Image>
+
+      {testData?.length && (
         <View
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            position: 'absolute',
+            bottom: height * 0.1,
+            right: width * 0.4,
+            height: 'auto',
+            transform: [{scaleY: 0.85}, {skewX: '30deg'}],
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'flex-end',
+            justifyContent: 'center',
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>{props.counter}</Text>
-          </Section>
-          <Section title="See Your Changes">
-            {props.doubleArray?.length && (
-              <>
-                <Text style={styles.rpmBig}>
-                  {String(props.doubleArray[0])[0]}
-                </Text>
-                <Text style={styles.rpm}>
-                  {String(Math.round(props.doubleArray[0] / 10) * 10).slice(1)}
-                </Text>
-              </>
-            )}
-          </Section>
-          <Section title="Debug">
-            is ecu connected: {`${props.isConnected}`}
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <Text style={styles.rpmBig}>{String(testData[0])[0]}</Text>
+          <Text style={styles.rpm}>
+            {String(Math.round(testData[0] / 10) * 10).slice(1)}
+          </Text>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      )}
+    </View>
   );
 }
 
@@ -118,12 +157,13 @@ const styles = StyleSheet.create({
     fontWeight: '700',
   },
   rpm: {
-    fontFamily: 'TechnicalStandardVP-Oblique',
+    fontFamily: 'TechnicalStandardVP-Medium',
     fontSize: 70,
     color: '#ddd',
+    lineHeight: 100,
   },
   rpmBig: {
-    fontFamily: 'TechnicalStandardVP-Oblique',
+    fontFamily: 'TechnicalStandardVP-Medium',
     fontSize: 100,
     color: '#ddd',
   },
