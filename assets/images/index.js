99178afe512be07a1e1cbe5d205efca6cf33c57c
Robert Kaurin <robykaurin@gmail.com>
2024-05-09 16:23:38 +0200

    add ambilight image assets

--- assets/images/index.js
+++ assets/images/index.js
@@ -1,3 +1,10 @@
 export const background = require('./gauge_bg.png');
+export const systemBg = require('./background.png');
 export const gauge = require('./gauge.png');
 export const gradient = require('./gradient.png');
+export const topDownCar = require('./test-2.png');
+
+export const area1 = require('./area-1.png');
+export const area2 = require('./area-2.png');
+export const area3 = require('./area-3.png');
+export const area4 = require('./area-4.png');
